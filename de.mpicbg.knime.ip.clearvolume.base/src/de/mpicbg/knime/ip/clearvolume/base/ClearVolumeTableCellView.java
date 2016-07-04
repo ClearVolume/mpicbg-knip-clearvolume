9ccbfb749ff8ad1a406ff15a241c1c6f9d090aa2
dietzc <dietzc85@googlemail.com>
2016-07-04 15:47:54 +0200

    refactoring to use new cell viewer

--- de.mpicbg.knime.ip.clearvolume.base/src/de/mpicbg/knime/ip/clearvolume/base/ClearVolumeTableCellView.java
+++ de.mpicbg.knime.ip.clearvolume.base/src/de/mpicbg/knime/ip/clearvolume/base/ClearVolumeTableCellView.java
@@ -1,33 +1,29 @@
 package de.mpicbg.knime.ip.clearvolume.base;
 
 import java.awt.BorderLayout;
-import java.awt.Component;
 import java.awt.Image;
+import java.util.List;
 
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
+import org.knime.core.data.DataValue;
+import org.knime.knip.base.data.img.ImgPlusValue;
+import org.knime.knip.cellviewer.interfaces.CellView;
+
+import de.mpicbg.jug.clearvolume.gui.GenericClearVolumeGui;
+import de.mpicbg.jug.plugins.ClearVolumePlugin;
 import net.imagej.ImgPlus;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 
-import org.knime.core.data.DataValue;
-import org.knime.core.node.config.ConfigRO;
-import org.knime.core.node.config.ConfigWO;
-import org.knime.knip.base.data.img.ImgPlusValue;
-import org.knime.knip.base.nodes.view.TableCellView;
-
-import de.mpicbg.jug.clearvolume.gui.GenericClearVolumeGui;
-import de.mpicbg.jug.plugins.ClearVolumePlugin;
-
 /**
  * @author jug
  */
-public class ClearVolumeTableCellView<T extends RealType<T> & NativeType<T>> implements TableCellView {
-
-    private ImgPlus<T> imgPlus;
+public class ClearVolumeTableCellView<T extends RealType<T> & NativeType<T>> implements CellView {
 
     private JPanel mainPanel;
+
     private GenericClearVolumeGui<T> panelGui;
 
     private DataValue oldValueToView;
@@ -38,35 +34,16 @@ public class ClearVolumeTableCellView<T extends RealType<T> & NativeType<T>> imp
      * {@inheritDoc}
      */
     @Override
-    public String getDescription() {
-        return "ClearVolume-KNIME integration.\n"
-                + "The viewer is capable of CUDA rendering of ImgPlus images. "
-                + "If CUDA is not available a OpenCL render will be used.\n"
-                + "For more information see 'https://bitbucket.org/clearvolume/clearvolume/wiki/Home'.\n"
-                + "Credits to: Loic Royer, Martin Weigert, Ulrik Guenther, and Florian Jug.";
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public String getName() {
-        return "ClearVolume";
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Component getViewComponent() {
+    public JPanel getViewComponent() {
         System.out.println("--== GET VIEW COMPONENT ==--");
         mainPanel = new JPanel(new BorderLayout());
-        try{
+        try {
             appicon = GenericClearVolumeGui.getCurrentAppIcon();
         } catch (Exception e) {
-//            NodeLogger.getLogger("ClearVolumeTableCellView").error("ClearVolume could not read the current app icon! Check linked com.apple.eawt version...");
-//            NodeLogger.getLogger("ClearVolumeTableCellView").error("Stacktrace:\n"+e.toString());
-            System.err.println("ClearVolume could not read the current app icon! Check linked com.apple.eawt version...");
+            //            NodeLogger.getLogger("ClearVolumeTableCellView").error("ClearVolume could not read the current app icon! Check linked com.apple.eawt version...");
+            //            NodeLogger.getLogger("ClearVolumeTableCellView").error("Stacktrace:\n"+e.toString());
+            System.err
+                    .println("ClearVolume could not read the current app icon! Check linked com.apple.eawt version...");
             e.printStackTrace();
         }
         return mainPanel;
@@ -76,18 +53,18 @@ public class ClearVolumeTableCellView<T extends RealType<T> & NativeType<T>> imp
      * {@inheritDoc}
      */
     @Override
-    public void updateComponent(final DataValue valueToView) {
+    public void updateComponent(final List<DataValue> valueToView) {
         System.out.println("--== UPDATE COMPONENT ==--");
 
         // if the currently shown imgPlus is the same as the one we get now
-        if (panelGui!=null && oldValueToView==valueToView) {
+        if (panelGui != null && oldValueToView == valueToView.get(0)) {
             // do nothing
             return;
         }
-        oldValueToView = valueToView;
+        oldValueToView = valueToView.get(0);
 
         // Build a new one and show it
-        final ImgPlus<T> imgPlus = ((ImgPlusValue<T>)valueToView).getImgPlus();
+        final ImgPlus<T> imgPlus = ((ImgPlusValue<T>)valueToView.get(0)).getImgPlus();
 
         boolean canBeShown = ClearVolumePlugin.checkIfShowable(mainPanel, imgPlus, true);
 
@@ -104,36 +81,28 @@ public class ClearVolumeTableCellView<T extends RealType<T> & NativeType<T>> imp
 
                     @Override
                     public void run() {
-//                        ClearVolumeSplashFrame splash = new ClearVolumeSplashFrame();
+                        //                        ClearVolumeSplashFrame splash = new ClearVolumeSplashFrame();
 
                         panelGui = new GenericClearVolumeGui<T>(imgPlus, 768, false);
                         mainPanel.add(panelGui, BorderLayout.CENTER);
                         mainPanel.validate();
 
-//                        splash.dispose();
+                        //                        splash.dispose();
 
-                        GenericClearVolumeGui.setCurrentAppIcon( appicon );
+                        GenericClearVolumeGui.setCurrentAppIcon(appicon);
                     }
                 };
 
-                if ( javax.swing.SwingUtilities.isEventDispatchThread() ) {
+                if (javax.swing.SwingUtilities.isEventDispatchThread()) {
                     todo.run();
                 } else {
-                    SwingUtilities.invokeLater( todo );
+                    SwingUtilities.invokeLater(todo);
                 }
-            } catch ( final Exception e ) {
-                System.err.println( "Relaunching CV session was interrupted in GenericClearVolumeGui!" );
+            } catch (final Exception e) {
+                System.err.println("Relaunching CV session was interrupted in GenericClearVolumeGui!");
                 e.printStackTrace();
             }
         }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void loadConfigurationFrom(final ConfigRO config) {
-        // forget it for now
     }
 
     /**
@@ -160,14 +129,6 @@ public class ClearVolumeTableCellView<T extends RealType<T> & NativeType<T>> imp
             panelGui.setVisible(false);
             mainPanel.remove(panelGui);
         }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void saveConfigurationTo(final ConfigWO config) {
-        // no worries
     }
 
 }
--- de.mpicbg.knime.ip.clearvolume.base/src/de/mpicbg/knime/ip/clearvolume/base/ClearVolumeViewer.java
+++ de.mpicbg.knime.ip.clearvolume.base/src/de/mpicbg/knime/ip/clearvolume/base/ClearVolumeViewer.java
@@ -1,23 +1,47 @@
 package de.mpicbg.knime.ip.clearvolume.base;
 
-import net.imglib2.type.NativeType;
-import net.imglib2.type.numeric.RealType;
+import java.util.List;
 
 import org.knime.core.data.DataValue;
 import org.knime.knip.base.data.img.ImgPlusValue;
-import org.knime.knip.base.nodes.view.TableCellView;
-import org.knime.knip.base.nodes.view.TableCellViewFactory;
+import org.knime.knip.cellviewer.interfaces.CellViewFactory;
 
-public class ClearVolumeViewer<T extends RealType<T> & NativeType<T>> implements TableCellViewFactory {
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.RealType;
+
+public class ClearVolumeViewer<T extends RealType<T> & NativeType<T>> implements CellViewFactory {
 
     @Override
-    public TableCellView[] createTableCellViews() {
-        return new TableCellView[]{new ClearVolumeTableCellView<T>()};
+    public ClearVolumeTableCellView<T> createCellView() {
+        return new ClearVolumeTableCellView<T>();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getCellViewName() {
+        return "ClearVolume-KNIME integration.\n" + "The viewer is capable of CUDA rendering of ImgPlus images. "
+                + "If CUDA is not available a OpenCL render will be used.\n"
+                + "For more information see 'https://bitbucket.org/clearvolume/clearvolume/wiki/Home'.\n"
+                + "Credits to: Loic Royer, Martin Weigert, Ulrik Guenther, and Florian Jug.";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getCellViewDescription() {
+        return "ClearVolume";
     }
 
     @Override
-    public final Class<? extends DataValue> getDataValueClass() {
-        return ImgPlusValue.class;
+    public boolean isCompatible(final List<Class<? extends DataValue>> values) {
+        if (values.size() == 1 && ImgPlusValue.class.isAssignableFrom(values.get(0))) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
